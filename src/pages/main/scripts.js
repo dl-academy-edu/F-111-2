a648ad698c883b610383a9d455071109f786cb69
Oleg Bugaichuk <olegussek@gmail.com>
2022-05-04 21:19:51 +0400

    6

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,25 +1,43 @@
-var popupBtn = document.querySelector('.button_js');
-var modal = document.querySelector('.modal');
-var popupClose =document.querySelector('.close');
-var input = modal.querySelector('input');
+var bannerBtn = document.querySelector('.banner__button');
+var popup = document.querySelector('.popup');
+var closePopup = document.querySelector('.popup__close');
+var input = popup.querySelector('input');
 
-popupBtn.addEventListener('click', function() {
-    modal.classList.add('modal-open');
+
+bannerBtn.addEventListener('click', function() {
+    popup.classList.add('popup_open');
     input.focus();
 })
 
-popupClose.addEventListener('click', function() {
-    modal.classList.remove('modal-open');
-    popupBtn.focus();
+closePopup.addEventListener('click', function() {
+    popup.classList.remove('popup_open');
+    bannerBtn.focus();
 })
 
-window.addEventListener('keydown', function(event) {
-    // console.log(event.key);
-    if (event.key === "Escape") {
-        // if (modal.classList.contains('modal-open')) {
-            modal.classList.remove('modal-open');
-            popupBtn.focus();
+window.addEventListener('keydown',function(event) {
+    // console.log(event.code);
+    if (event.code === "Escape" && popup.classList.contains('popup_open')) {
+        // if(popup.classList.contains('popup-open')) {
+            popup.classList.remove('popup_open');
+            bannerBtn.focus();
         // }
-        
     }
+})
+
+
+var buttonMobileHeader = document.querySelector('.header__humburger');
+var buttonCloseMenu = document.querySelector('.mobile__close');
+var mobileHeader = document.querySelector('.mobile');
+
+console.log(buttonMobileHeader)
+
+
+buttonMobileHeader.addEventListener('click', function() {
+    mobileHeader.classList.add('mobile__open');
+    console.log("sdfsdfsdf");
+})
+
+buttonCloseMenu.addEventListener('click', function() {
+    mobileHeader.classList.remove('mobile__open');
+    console.log("11");
 })
